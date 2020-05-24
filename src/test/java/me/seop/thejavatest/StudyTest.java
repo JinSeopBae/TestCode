a2c06b5ecff77cedef7b51d4122e1c4d4d90e0e3
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-05-25 07:34:48 +0900

    junit5 naming

--- src/test/java/me/seop/thejavatest/StudyTest.java
+++ src/test/java/me/seop/thejavatest/StudyTest.java
@@ -1,12 +1,17 @@
 package me.seop.thejavatest;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
+    @DisplayName("스터디 만들기 \uD83D\uDE31")
     void create() {
         Study study = new Study();
         assertNotNull(study);
