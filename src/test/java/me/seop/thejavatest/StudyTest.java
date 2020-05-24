953a782681cd93b36c72368b94e7a915c8583de8
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-05-25 07:25:57 +0900

    junit5 start

---
+++ src/test/java/me/seop/thejavatest/StudyTest.java
@@ -0,0 +1,14 @@
+package me.seop.thejavatest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudyTest {
+
+    @Test
+    void create() {
+        Study study = new Study();
+        assertNotNull(study);
+    }
+}
