336fc6c972f832e5e45cde31e5bd0b809bda1051
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-06-02 09:07:55 +0900

    mock stubbing˚

--- src/test/java/me/seop/thejavatest/study/StudyServiceTest.java
+++ src/test/java/me/seop/thejavatest/study/StudyServiceTest.java
@@ -17,6 +17,9 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
@@ -33,5 +36,23 @@ class StudyServiceTest {
 
         StudyService studyService = new StudyService(memberService,studyRepository);
         assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("hazard@email.com");
+
+        when(memberService.findById(any()))
+                .thenReturn(Optional.of(member))
+                .thenThrow(new RuntimeException())
+                .thenReturn(Optional.empty());
+
+        Optional<Member> byId = memberService.findById(1L);
+        assertEquals("hazard@email.com",byId.get().getEmail());
+
+        assertThrows(RuntimeException.class, () -> {
+            memberService.findById(2L);
+        });
+
+        assertEquals(Optional.empty(), memberService.findById(3L));
     }
 }
